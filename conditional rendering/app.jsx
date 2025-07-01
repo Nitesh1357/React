5c59f19094ad25f6a00fa12bea0190c957bfd7a1
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-07-01 16:26:00 +0530

    Create app.jsx

---
+++ conditional rendering/app.jsx
@@ -0,0 +1,17 @@
+return (<MyComponent/>);
+  return (
+    <div>
+      <MyComponent/>
+       <h2>My Project Home</h2>
+      <Greeting />
+      <h1>Product List</h1>
+      <Product name="buds" Price = {1989} />
+      <Product name="Samsung phone" Price ={21000} />
+      <Product name="Mixer" Price = {3200} />
+      <Product name="Pan" Price = {2400} />
+      <Product name="Vivo phone" Price = {20000} />
+    </div>
+
+  );
+
+export default App;

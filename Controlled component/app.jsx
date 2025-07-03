423280024d1d8e81e91155d235582657f3d4a925
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-07-03 15:57:22 +0530

    Create app.jsx

---
+++ Controlled component/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ControlledLoginForm from './ControlledLoginForm';
+
+function App() {
+  return (
+    <div>
+      <ControlledLoginForm />
+    </div>
+  );
+}
+
+export default App;
